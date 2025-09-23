1767fbb55d64ed0201c793dcc0b459a327614af9
AnchalSingh915 <anchalsingh8736@gmail.com>
2025-09-23 18:58:33 +0530

    Add files via upload

---
+++ for17c.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class for17c
+{
+    public static void main(String ar[])
+    {
+       int i=0,s=0;
+       for(i=1;i<=9;i++)
+         s=s+i*(i+2);
+         System.out.println(s);
+    }    
+}
