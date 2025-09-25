cde693b1eaa2e3077798865a5c18262abef49c6a
Anchal Singh <anchalsingh8736@gmail.com>
2025-09-25 19:21:58 +0530

    Add files via upload

--- while10.java
+++ while10.java
@@ -3,28 +3,22 @@ class while10
 {
     public static void main(String args[])
     {
-        int x=0,y=1,a=1,z=0,l=0,u=0;
+        int x=0,y=1,a=1,z=0,l=0,m=3;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the no.");
+        System.out.println("enter the terms");
         l=sc.nextInt();
-        if(y<=l)
+        if(m<l)
         System.out.print(x+" "+y+" "+a);
         z=x+y+a;
-        u=x+y+a;
-        while(z<=l)
+        while(m<l)
         {
-            if(z<=l)
-            {
-                System.out.print(" "+z);
-                u=u+z;
-            }
+            if(m<l)
+            System.out.print(" "+z);
             x=y;
             y=a;
             a=z;
             z=x+y+a;
+            m++;
         }
-        System.out.println();
-        System.out.println("total sum is"+u);
     }
-}    
-        
+}
